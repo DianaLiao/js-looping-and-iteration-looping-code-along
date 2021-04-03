f4a56f479b793498a1ae82981d639d38b7695bbe
Diana Liao <dijali@gmail.com>
2021-04-03 18:40:10 -0400

    commit tree 753c32eda9fcdb0b0c5a1c1c5fc003f701a84d03

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+  let messages = []
+  for (let i=0; i < names.length; i++ ){
+    messages[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+  }
+  return messages;
+}
+
+function countDown(int){
+  while (int >= 0){
+    console.log(int);
+    int--;
+  }
+}
